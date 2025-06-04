dab3ed33cf05a3f7209439ea420108a1f0ac6102
유희 <emop99@naver.com>
2025-06-05 01:00:06 +0900

    Update middleware to use environment variable for redirect URLs

--- middleware.js
+++ middleware.js
@@ -22,15 +22,15 @@ export async function middleware(request) {
 
   // 인증되지 않은 사용자라면 로그인 페이지로 리디렉션
   if (!token) {
-    const url = new URL('/auth/signin', request.nextUrl.href);
-    url.searchParams.set('callbackUrl', encodeURI(request.nextUrl.href));
+    const url = new URL('/auth/signin', `${process.env.NEXT_PUBLIC_SITE_URL}${request.nextUrl.pathname}`);
+    url.searchParams.set('callbackUrl', encodeURI(`${process.env.NEXT_PUBLIC_SITE_URL}${request.nextUrl.pathname}`));
     return NextResponse.redirect(url);
   }
 
   // 어드민 권한이 아니라면 (grade가 'ADMIN'이 아니라면) 메인 페이지로 리디렉션
   // grade 값은 실제 DB 구조에 따라 변경 필요
   if (token.grade !== 'ADMIN') {
-    return NextResponse.redirect(new URL('/', request.nextUrl.href));
+    return NextResponse.redirect(new URL('/', `${process.env.NEXT_PUBLIC_SITE_URL}${request.nextUrl.pathname}`));
   }
 
   // 어드민 권한이 확인되면 요청 허용
