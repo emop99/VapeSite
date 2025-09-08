b86287b3e23a094465a6bab21cb17a97362da912
유희 <emop99@naver.com>
2025-09-08 21:55:38 +0900

    Update ranking page icon and improve login prompt message

--- pages/ranking.js
+++ pages/ranking.js
@@ -141,9 +141,12 @@ export default function Ranking() {
         <div className="mt-4 sm:mt-0">
           <div className="text-sm text-gray-500">
             <span className="inline-flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1 text-accent" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v4a2 2 0 01-2 2H9a2 2 0 01-2-2z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1 text-accent" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
+                </path>
               </svg>
               랭킹 데이터를 보시려면 로그인이 필요합니다
             </span>
