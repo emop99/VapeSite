7ed32a66261a734c6d4a3e436bf899432dbc7a59
유희 <emop99@naver.com>
2025-12-09 20:53:54 +0900

    Adjust Google AdSense script loading strategy in `_app.js` to `beforeInteractive` for earlier initialization

--- pages/_app.js
+++ pages/_app.js
@@ -83,7 +83,7 @@ function MyApp({ Component, pageProps }) {
           />
           <Script
             id="google-ads"
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4259248617155600`}
             crossOrigin="anonymous"
