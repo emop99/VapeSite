e6fa2e7b46711d2dd7d7054003348956fffbf7e4
유희 <emop99@naver.com>
2026-01-31 16:51:51 +0900

    Integrate Microsoft Clarity for analytics: add Clarity script alongside existing Google AdSense in `_app.js`.

--- pages/_app.js
+++ pages/_app.js
@@ -81,16 +81,29 @@ function MyApp({ Component, pageProps }) {
           `
             }}
           />
+          {/* Microsoft Clarity */}
+          <Script
+            id="clarity-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "v9ybyazg4y");
+              `
+            }}
+          />
+          {/* Google AdSense */}
+          <Script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4259248617155600"
+              crossOrigin="anonymous"
+              strategy="afterInteractive"
+          />
         </>
       ) : ``}
-
-      {/* Google AdSense */}
-      <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4259248617155600"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-      />
 
       {/* PWA 설치 프롬프트 */}
       <PWAInstallPrompt/>
